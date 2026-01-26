cd7e68f61de0827cec05de7d2e15c9ed62276c3f
developerbayazid <bayazid.freelancer@gmail.com>
2026-01-27 01:55:16 +0600

    price, quantity, total realtime state update while purhcasing

--- app/Filament/Resources/Purchases/Schemas/PurchaseForm.php
+++ app/Filament/Resources/Purchases/Schemas/PurchaseForm.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\Unit;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\KeyValue;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
@@ -33,80 +34,89 @@ class PurchaseForm
                         DatePicker::make('purchase_date')
                             ->required(),
 
-                        // TextInput::make('total')
-                        //     ->required()
-                        //     ->numeric(),
-                        // TextInput::make('discount')
-                        //     ->required()
-                        //     ->numeric()
-                        //     ->default(0.0),
-                        // TextInput::make('remarks'),
-                        // KeyValue::make('data')
-                        // ->columnSpanFull(),
                     ]),
                 Section::make('Product Details')
-                        ->columns(4)
                         ->components([
-                            Select::make('product_id')
-                                ->required()
-                                ->options(function () {
-                                    return Product::pluck('name', 'id')->toArray();
-                                })
-                                ->searchable()
-                                ->createOptionForm(function (Schema $schema) {
-                                    return $schema
-                                        ->columns(1)
-                                        ->components([
-                                            TextInput::make('name')
-                                            ->label('Product Name')
-                                            ->required(),
-                                            Select::make('category_id')
-                                                ->options(function () {
-                                                    return Category::pluck('name', 'id')->toArray();
-                                                })
-                                                ->required(),
-                                            TextInput::make('sku')
-                                                ->label('Product Code')
-                                                ->unique(ignoreRecord: true)
-                                                ->helperText('Unique identifier for this product.')
-                                                ->required(),
-                                            TextInput::make('quantity')
-                                                ->required()
-                                                ->numeric()
-                                                ->default(1.0),
-                                            TextInput::make('price')
-                                                ->required()
-                                                ->numeric()
-                                                ->default(0.0)
-                                                ->prefix('$'),
-                                            TextInput::make('safety_stock')
-                                                ->required()
-                                                ->numeric()
-                                                ->helperText('Safety stock level to trigger restocking')
-                                                ->default(0),
-                                            Select::make('unit_id')
-                                                ->options(function () {
-                                                    return Unit::pluck('name', 'id')->toArray();
-                                                })
-                                                ->required(),
-                                    ]);
+                            Repeater::make('product')
+                                ->label('Product')
+                                ->columns(4)
+                                ->components([
+                                    Select::make('product_id')
+                                        ->required()
+                                        ->options(function () {
+                                            return Product::pluck('name', 'id')->toArray();
+                                        })
+                                        ->searchable()
+                                        ->createOptionForm(function (Schema $schema) {
+                                            return $schema
+                                                ->columns(1)
+                                                ->components([
+                                                    TextInput::make('name')
+                                                    ->label('Product Name')
+                                                    ->required(),
+                                                    Select::make('category_id')
+                                                        ->options(function () {
+                                                            return Category::pluck('name', 'id')->toArray();
+                                                        })
+                                                        ->required(),
+                                                    TextInput::make('sku')
+                                                        ->label('Product Code')
+                                                        ->unique(ignoreRecord: true)
+                                                        ->helperText('Unique identifier for this product.')
+                                                        ->required(),
+                                                    TextInput::make('quantity')
+                                                        ->required()
+                                                        ->numeric()
+                                                        ->default(1.0),
+                                                    TextInput::make('price')
+                                                        ->required()
+                                                        ->numeric()
+                                                        ->default(0.0)
+                                                        ->prefix('$'),
+                                                    TextInput::make('safety_stock')
+                                                        ->required()
+                                                        ->numeric()
+                                                        ->helperText('Safety stock level to trigger restocking')
+                                                        ->default(0),
+                                                    Select::make('unit_id')
+                                                        ->options(function () {
+                                                            return Unit::pluck('name', 'id')->toArray();
+                                                        })
+                                                        ->required(),
+                                            ]);
 
-                                }),
-                                TextInput::make('price')
-                                    ->required()
-                                    ->numeric()
-                                    ->default(0.0)
-                                    ->prefix('$'),
-                                TextInput::make('quantity')
-                                    ->required()
-                                    ->numeric()
-                                    ->default(1.0),
-                                TextInput::make('total')
-                                    ->required()
-                                    ->numeric()
-                                    ->default(0.0)
-                                    ->prefix('$'),
+                                        }),
+                                        TextInput::make('price')
+                                            ->required()
+                                            ->numeric()
+                                            ->default(0.0)
+                                            ->prefix('$')
+                                            ->reactive()
+                                            ->afterStateUpdated(function(callable $get, callable $set) {
+                                                $price = $get('price') ?? 0;
+                                                $quantity = $get('quantity') ?? 1;
+                                                $total = $quantity * $price;
+                                                $set('total', $total);
+                                            }),
+                                        TextInput::make('quantity')
+                                            ->required()
+                                            ->numeric()
+                                            ->default(1.0)
+                                            ->reactive()
+                                            ->afterStateUpdated(function(callable $get, callable $set) {
+                                                $price = $get('price') ?? 0;
+                                                $quantity = $get('quantity') ?? 1;
+                                                $total = $quantity * $price;
+                                                $set('total', $total);
+                                            }),
+                                        TextInput::make('total')
+                                            ->required()
+                                            ->numeric()
+                                            ->default(0.0)
+                                            ->prefix('$')
+                                            ->disabled(),
 
+                                    ]),
                     ]),
             ]);
     }
