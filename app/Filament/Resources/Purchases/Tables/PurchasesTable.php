704ade5d7f7284b1a0d7cf0723c63d31433ec9c1
developerbayazid <bayazid.freelancer@gmail.com>
2026-02-05 19:08:03 +0600

    Add two factor auth

--- app/Filament/Resources/Purchases/Tables/PurchasesTable.php
+++ app/Filament/Resources/Purchases/Tables/PurchasesTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Purchases\Tables;
 
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
@@ -49,6 +50,9 @@ class PurchasesTable
             ])
             ->recordActions([
                 EditAction::make(),
+                Action::make('make_invoice')
+                    ->label('View Invoice')
+                    ->icon('heroicon-o-document')
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,7 +11,9 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
+// use Filament\Support\Colors\Color;
+use Openplain\FilamentShadcnTheme\Color;
+
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -33,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->brandName('Inventory Management')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Default,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
